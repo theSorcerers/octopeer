b06535a95b7f3a7497cd390f554f7534049635d5
Aaron Ang <awz.ang@gmail.com>
2016-05-31 20:28:45 +0200

    Remove /admin/ from URLs

--- octopeer/urls.py
+++ octopeer/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url, include
 from core import views
-from django.contrib import admin
+from django.views.generic import RedirectView
 
 urlpatterns = [
     url(r'^api/', include('core.urls')),
-    url(r'^admin/', admin.site.urls),
     url(r'^docs/', include('rest_framework_docs.urls')),
+    url(r'^$', RedirectView.as_view(url='/api/')),
 ]
