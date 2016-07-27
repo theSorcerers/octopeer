f3b1822c10a43f7d87718ddd7b2fd5be8dcbc080
Aaron Ang <awz.ang@gmail.com>
2016-07-27 16:32:17 +0200

    Delete all Django files

--- octopeer/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url, include
-from core import views
-from django.views.generic import RedirectView
-
-urlpatterns = [
-    url(r'^api/', include('core.urls')),
-    url(r'^docs/', include('rest_framework_docs.urls')),
-    url(r'^$', RedirectView.as_view(url='/api/')),
-]
