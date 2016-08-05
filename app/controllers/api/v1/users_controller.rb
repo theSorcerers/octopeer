89a2683bc9362cff637d85739f338885c0291348
Aaron Ang <awz.ang@gmail.com>
2016-08-05 03:08:08 +0200

    Add create and show action for user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,8 +4,21 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def show
+    render json: User.find(params[:id])
   end
 
   def create
+    user = User.new(user_params)
+    if user.save
+      render json: user
+    else
+      render json: { errors: user.errors }
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username)
   end
 end
