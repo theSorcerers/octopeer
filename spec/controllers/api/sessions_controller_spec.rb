071a5c4984b96719f936fad697e857ed50d522c9
Aaron Ang <awz.ang@gmail.com>
2016-08-07 23:56:53 +0200

    Test session API endpoints

--- spec/controllers/api/sessions_controller_spec.rb
+++ spec/controllers/api/sessions_controller_spec.rb
@@ -1,26 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Api::SessionsController, type: :controller do
-
   describe "GET #index" do
-    it "returns http success" do
+    it "returns all sessions" do
+      create_list :session, 2
+
       get :index
-      expect(response).to have_http_status(:success)
+
+      expect(json.length).to eq(2)
     end
   end
 
   describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
+    it "returns detail for one session" do
+      session = create :session
+
+      get :show, params: { id: session.id }
+
+      expect(json[:id]).to eq(session.id)
+      expect(json[:pull_request][:id]).to eq(session.pull_request_id)
+      expect(json[:user][:id]).to eq(session.user_id)
     end
   end
 
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+  describe "POST #create" do
+    it "creates a session" do
+      pull_request = create :pull_request
+      user = create :user
+
+      post :create,
+           params: { session: { pull_request_id: pull_request.id,
+                                user_id: user.id } }
+
+      expect(json).to include(:id)
+      expect(json[:pull_request][:id]).to eq(pull_request.id)
+      expect(json[:user][:id]).to eq(user.id)
     end
   end
-
 end
---
+++ spec/factories/sessions.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :session do
+    pull_request
+    user
+  end
+end
